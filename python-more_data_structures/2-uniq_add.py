cf241812ad6b56246452140b845ab34bfe311646
jonathan-k05 <rakotondrasoa.jonathan@gmail.com>
2026-05-19 17:17:56 +0300

    task 02

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,5 +4,5 @@ def uniq_add(my_list=[]):
     for val in my_list:
         if val not in uniques:
             uniques.append(val)
-            total = sum([val for val in uniques])
+    total = sum(uniques)
     return total
