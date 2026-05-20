c2784090b268b6c00291017be998c50e1a0eec8e
jonathan-k05 <rakotondrasoa.jonathan@gmail.com>
2026-05-20 22:58:50 +0300

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+              'C': 100, 'D': 500, 'M': 1000}
+
+    result = 0
+    for i in range(len(roman_string)):
+        current = values[roman_string[i]]
+        next_val = values[roman_string[i + 1]] if i + 1 < len(roman_string) else 0
+
+        if current < next_val:
+            result -= current
+        else:
+            result += current
+
+    return result
