8bfa56f8d99414d340b0991956894c7c45a44844
jonathan-k05 <rakotondrasoa.jonathan@gmail.com>
2026-05-20 11:47:49 +0300

    task 03

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     val_final = []
-    val_final += [val for val in set_1 for val2 in set_2 if val==val2]
+    val_final += [val for val in set_1 for val2 in set_2 if val == val2]
     return val_final
