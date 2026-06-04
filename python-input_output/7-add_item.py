6746e11fc749b203cbdf4c44879f7a853604c6cc
jonathan-k05 <rakotondrasoa.jonathan@gmail.com>
2026-06-04 15:18:34 +0300

    task07

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,9 @@ and saves them to a JSON file format.
 import sys
 import os
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+save_to_json_file = (
+    __import__('5-save_to_json_file').save_to_json_file
+)
 load_from_json_file = (
     __import__('6-load_from_json_file').load_from_json_file
 )
