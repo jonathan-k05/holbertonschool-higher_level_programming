2850ae12f42c9834467007b737566f483e411372
jonathan-k05 <rakotondrasoa.jonathan@gmail.com>
2026-06-04 15:17:28 +0300

    task07

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,7 +7,9 @@ import sys
 import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = (
+    __import__('6-load_from_json_file').load_from_json_file
+)
 
 filename = "add_item.json"
 
