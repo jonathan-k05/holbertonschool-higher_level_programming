6829c4915c175c48e933a2ad72a1a49a5c1442e5
jonathan-k05 <rakotondrasoa.jonathan@gmail.com>
2026-06-04 09:59:09 +0300

    task 02

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+It includes type and value validation for instantiation.
+"""
+
+
 class Square:
+    """A class that defines a square by its size."""
+
     def __init__(self, size=0):
-        if type(size) != int:
+        """Initializes a new Square instance.
+
+        Args:
+            size (int): The length of a side of the square. Defaults to 0.
+        """
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
