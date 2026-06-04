4d3f66369d584b062ddc472622dde5b25ac68412
jonathan-k05 <rakotondrasoa.jonathan@gmail.com>
2026-06-04 10:22:21 +0300

    task 00

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines an empty Square class.
+It serves as the baseline for learning Object-Oriented Programming in Python.
+"""
+
+
 class Square:
+    """An empty class that defines a square."""
     pass
